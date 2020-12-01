3bbdfc16635ed1115571022d8df57e78b9c413d7
Willmish <willmiszgaming@gmail.com>
2020-12-01 20:20:57 +0100

    Add simple labels.

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.font as font
 
 # Create a GUI that interfaces with SimAVR
 class MainFrame(tkinter.Frame):
@@ -13,15 +14,47 @@ class View:
         self.controller = None
 
     def create_gui(self):
-        self.title = tkinter.Label(self.root, text="Bioreactor app control")
+        self.title_font = font.Font(family="Verdana", size=15, weight="bold")
+        self.title = tkinter.Label(self.root, text="Bioreactor app control", height=2, font=self.title_font)
+      
+        # Temperature subsystem
+        self.temperature_title = tkinter.Label(self.root, text="Temperature subsystem:", height=1)
+        self.temperature_target_title = tkinter.Label(self.root, text="Target value: ") 
+        self.temperature_target_value = tkinter.Label(self.root, text="0.0") 
+        self.temperature_current_title = tkinter.Label(self.root, text="Current value: ") 
+        self.temperature_current_value = tkinter.Label(self.root, text="0.0") 
         
-        self.temperature_title = tkinter.Label(self.root, text="Temperature subsystem:")
+        # Stiring subsystem
+        self.stiring_title = tkinter.Label(self.root, text="Stiring subsystem:", height=1)
+        self.stiring_target_title = tkinter.Label(self.root, text="Target value: ") 
+        self.stiring_target_value = tkinter.Label(self.root, text="0.0") 
+        self.stiring_current_title = tkinter.Label(self.root, text="Current value: ") 
+        self.stiring_current_value = tkinter.Label(self.root, text="0.0") 
+
+        # PH subsystem
+        self.ph_title = tkinter.Label(self.root, text="PH subsystem:", height=1)
+        self.ph_target_title = tkinter.Label(self.root, text="Target value: ") 
+        self.ph_target_value = tkinter.Label(self.root, text="0.0") 
+        self.ph_current_title = tkinter.Label(self.root, text="Current value: ") 
+        self.ph_current_value = tkinter.Label(self.root, text="0.0") 
+        
 
         # Pack the widgets at the end
         self.pack_gui()
 
     def pack_gui(self):
         self.title.pack()
+        self.temperature_title.pack(side=tkinter.TOP, anchor=tkinter.NW)
+        self.temperature_target_value.pack(side=tkinter.TOP, anchor=tkinter.NW)
+        self.temperature_current_value.pack(side=tkinter.TOP, anchor=tkinter.NW)
+
+        self.stiring_title.pack(side=tkinter.TOP, anchor=tkinter.NW)
+        self.stiring_target_value.pack(side=tkinter.TOP, anchor=tkinter.NW)
+        self.stiring_current_value.pack(side=tkinter.TOP, anchor=tkinter.NW)
+
+        self.ph_title.pack(side=tkinter.TOP, anchor=tkinter.NW)
+        self.ph_target_value.pack(side=tkinter.TOP, anchor=tkinter.NW)
+        self.ph_current_value.pack(side=tkinter.TOP, anchor=tkinter.NW)
 
     def register_controller(self, controller):
         self.controller = controller
