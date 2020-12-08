c9b51c31054217bdae6046ef8dcddd030d9ade95
Oelin <42823429+Oelin@users.noreply.github.com>
2020-12-08 00:59:01 +0000

    Update sketch.ino

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -2,6 +2,8 @@
 #include "moderator.h"
 #include "ph.h"
 
+#define optimum 5.0
+
 
 void setup() {
   Serial.begin(9600);
@@ -10,8 +12,17 @@ void setup() {
 }
 
 
+Moderator *mod = moderator(1, 1, 1, optimum);
+moderator_clear(mod);
+
+
+float input, output;
+
 void loop() { 
-  d
+  input = measure_ph();
+  output = moderator_tick(mod, input);
+  
+  // pump acid or alkeli based on PID output
 }
 
 
