428cccc215aded382b687dc7ef4af56b95090a34
Oelin <oel1n@protonmail.com>
2020-12-09 22:17:31 +0000

    update

--- src/sketch/sketch.ino
+++ src/sketch/sketch.ino
@@ -1,28 +1,31 @@
 #include <stdio.h>
-#include "moderator.h"
-#include "ph.h"
+#include "pca9685.h"
 
+
+#define acid_on_reg 0x06
+#define acid_off_reg 0x08
+#define base_on_reg 0x0A
+#define base_off_reg 0x0C
+#define mode_reg 0x00
+#define ph_probe A1
 #define optimum 5.0
 
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Hello World");
   pinMode(ph_probe, INPUT);
+  write8(mode_reg, AUTOINC);
+
+  write16(acid_on_reg, 0x0199);
+  write16(acid_off_reg, 0x04CC);
+
+  write16(base_on_reg, 0x0000);
+  write16(base_off_reg, 0x0000);
 }
 
 
-Moderator *mod = moderator(1, 1, 1, optimum);
-moderator_clear(mod);
-
-
-float input, output;
-
 void loop() { 
-  input = measure_ph();
-  output = moderator_tick(mod, input);
-  
-  // pump acid or alkeli based on PID output
+
 }
 
 
